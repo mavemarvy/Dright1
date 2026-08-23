2b13dd4479864999ae30f5bb9692746fb6b96ffa
Mavemarvy <mvy00342@gmail.com>
2026-08-23 06:03:28 +0100

    Add city dependency API helper for signup

---
+++ public/signup-city-data.js
@@ -0,0 +1,5 @@
+/* DRIGHT1 city dependency helper. Loaded by signup.html when present. */
+window.DRIGHT_CITY_API = {
+  statesUrl: country => 'https://countriesnow.space/api/v0.1/countries/states/q?country=' + encodeURIComponent(country),
+  citiesUrl: 'https://countriesnow.space/api/v0.1/countries/state/cities/q'
+};
